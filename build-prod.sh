c4aa961e8d717f1bf2d8ff4eb0dffe2ae5c76387
Julian Ruiz Diaz <julian.rd7@gmail.com>
2019-10-23 11:03:02 -0300

    add auth and circle ci support

---
+++ build-prod.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t zoopix-unleash -f Dockerfile.prod .
--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 'use strict';
 
 const unleash = require('./lib/server-impl');
+// eslint-disable-next-line no-unused-vars
+const CognitoAuthenticator = require('./lib/auth/cognito');
+const sharedSecret = process.env.CLIENT_API_SECRET;
 
-unleash.start();
+unleash
+    .start({
+        enableLegacyRoutes: false,
+        preRouterHook: app => {
+            app.use('/api/client', (req, res, next) => {
+                if (req.header('authorization') === sharedSecret) {
+                    next();
+                } else {
+                    res.sendStatus(401);
+                }
+            });
+        },
+    })
+    .then(service => {
+        console.log(
+            `Unleash started on http://localhost:${service.app.get('port')}`
+        );
+    });
